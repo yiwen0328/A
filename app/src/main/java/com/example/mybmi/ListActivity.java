02689daec3aba347f4601849106963ff58d34cb1
CJCU <111B13910@mailst.cjcu.edu.tw>
2024-04-24 14:56:08 +0800

    Listview練習

--- app/src/main/java/com/example/mybmi/ListActivity.java
+++ app/src/main/java/com/example/mybmi/ListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -27,12 +28,24 @@ public class ListActivity extends AppCompatActivity {
         list.add("香蕉");
         list.add("橘子");
         list.add("西瓜");
+        list.add("芭樂");
+        list.add("梨子");
+        list.add("葡萄");
+        list.add("櫻桃");
+        list.add("草莓");
+        list.add("藍莓");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spfruit =  findViewById(R.id.spfruit);
         TextView tvshow1 = findViewById(R.id.tvshow1);
+        ListView lvfruit = findViewById(R.id.lvfruit);
+
         spfruit.setAdapter(adapter);
+        lvfruit.setAdapter(adapter);
 
         spfruit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -46,6 +59,12 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
+       lvfruit.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               tvshow1.setText(String.valueOf(position));
+           }
+       });
 
 
 
